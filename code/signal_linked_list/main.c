51c2383365bec960626ed06c2d880f33863c8a65
zhengxiaowai <h1x2y3awalm@gmail.com>
2016-11-25 00:40:59 +0800

    list

--- code/signal_linked_list/main.c
+++ code/signal_linked_list/main.c
@@ -4,7 +4,6 @@
 typedef struct Node
 {
     struct Node *next;
-    int key;
     int length;
     int data;
 } Node;
@@ -14,26 +13,82 @@ Node *create_list()
 {
     Node *head = NULL;
     head = (Node *)malloc(sizeof(Node));
-    head->length = 0;
     head->next = NULL;
+    head->length = 0;
 
     return head;
 }
 
-void list_insert(Node *head, int key, Node *new)
+void list_insert(Node *head, int pos, Node *new)
 {
-    if (head == NULL || new == NULL)
+    Node *tmp = head;
+    if (head == NULL || new == NULL || pos < 1)
     {
-        return head; 
+        return;
     }
 
-    if (key > head->length+1 || key == 0)
+    if (pos > tmp->length + 1)
     {
-        return NULL; 
+        return;
     }
 
+    //TODO
 
 
+}
+
+Node *list_search(Node *head, int pos)
+{
+    Node *tmp = head;
+    
+    int key = 0;
+    while(tmp != NULL && key != pos)
+    {
+        tmp = tmp->next;
+        key += 1;
+    }
+
+    return tmp;
+
+}
+
+
+// 删除分成两种
+// 1. 删除给定元素 O(1)
+// 2. 删除关键字元素 O(n)
+void list_delete(Node *head, int pos)
+{
+    if (head == NULL && head->next == NULL) {
+        return;
+    }
+    
+    if (pos < 1) {
+        return;
+    }
+    
+    Node *tmp = head;
+    if (pos == 1) {
+        // 直接删除第一个元素
+        Node *pos_1 = tmp->next;
+        if (pos_1->next != NULL)
+        {
+            // 存在第二个元素
+            tmp->next = pos_1->next;
+        }
+        free(pos_1);
+    }
+    else
+    {
+        Node *pos_prev = list_search(head, pos-1);
+        Node *phit = pos_prev->next;
+
+        if (phit->next != NULL)
+        {
+           pos_prev->next = phit->next; 
+        }
+
+        free(phit);
+    }
 }
 
 
