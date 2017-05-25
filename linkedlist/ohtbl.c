17f75cb6e80925150a998843f9c1d336d068c648
zhengxiaowai <h1x2y3awalm@gmail.com>
2017-05-25 17:56:22 +0800

    feat: ohtbl

---
+++ linkedlist/ohtbl.c
@@ -0,0 +1,166 @@
+#include <string.h>
+#include <stdio.h>
+#include "ohtbl.h"
+
+#define O_HASHTBL_POSITIONS 11
+
+static char vacated;
+
+int ohtbl_init(
+        OHTbl *htbl,
+        int positions,
+        int (*h1)(const void *key),
+        int (*h2)(const void *key),
+        int (*match)(const void *key1, const void *key2),
+        void (*destroy)(void *data))
+{
+    int i;
+
+    if ((htbl->table = (void **)malloc(positions * sizeof(void *))) == NULL)
+        return -1;
+    
+    for (i=0; i<positions; i++)
+        htbl->table[i] = NULL;
+
+    htbl->vacated = &vacated;
+
+    htbl->h1 = h1;
+    htbl->h2 = h2;
+    htbl->match = match;
+    htbl->destroy = destroy;
+    htbl->positions = positions;
+    htbl->size = 0;
+    
+    return 0;
+}
+
+void ohtbl_destroy(OHTbl *htbl)
+{
+    int i;
+
+    if (htbl->destroy != NULL)
+    {
+        for (i=0; i<htbl->positions; i++)
+        {
+            if (htbl->table[i] != NULL && htbl->table[i] != htbl->vacated) 
+                htbl->destroy(htbl->table[i]);
+        }
+    }
+
+    free(htbl->table);
+    memset(htbl, 0, sizeof(OHTbl));
+}
+
+int ohtbl_insert(OHTbl *htbl, const void *data)
+{
+    void *temp;
+    int i, position;
+    
+    // full
+    if (htbl->size == htbl->positions)
+        return -1;
+   
+    temp = (void *)data;
+    if (ohtbl_lookup(htbl, &temp) == 0)
+        return 1;
+    
+    for (i=0; i<htbl->positions; i++)
+    {
+        position = (htbl->h1(data) + (i * htbl->h2(data))) % htbl->positions;
+        if (htbl->table[position] == NULL || htbl->table[position] == htbl->vacated)
+        {
+            htbl->table[position] == (void *)data;
+            htbl->size++;
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+int ohtbl_remove(OHTbl *htbl, void **data)
+{
+    int i, position;
+
+    for (i=0; i<htbl->positions; i++)
+    {
+        position = (htbl->h1(data) + (i * htbl->h2(data))) % htbl->positions;
+
+        if (htbl->table[position] == NULL)
+            return -1;
+
+        if (htbl->table[position] == htbl->vacated)
+            continue;
+
+
+        if (htbl->match(htbl->table[position], *data))
+        {
+            *data = htbl->table[position];
+            htbl->table[position] == htbl->vacated;
+            htbl->size--;
+            return 0;
+        }
+    }
+
+    return -1;
+
+}
+
+int ohtbl_lookup(const OHTbl *htbl, void **data)
+{
+    int i, position;
+
+    for (i=0; i<htbl->positions; i++)
+    {
+        position = (htbl->h1(data) + (i * htbl->h2(data))) % htbl->positions;
+
+        if (htbl->table[position] == NULL)
+            return -1;
+        else if (htbl->match(htbl->table[position], *data))
+        {
+            *data = htbl->table[position];
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+int match(const void *k1, const void *k2)
+{
+    return *(int *)k1 == *(int *)k2;
+}
+
+int hash1(const void *key)
+{
+    return (*(const int *)key % O_HASHTBL_POSITIONS);
+}
+
+int hash2(const void *key)
+{
+    return (*(const int *)key % O_HASHTBL_POSITIONS - 2);
+}
+
+void ohtbl_print(OHTbl *htbl)
+{
+    int i;
+    for (i=0; i<htbl->positions; i++)
+    {
+        printf("%d: ", i);
+        if (htbl->table[i] == NULL)
+            printf("NULL\n");
+        else if (htbl->table[i] == htbl->vacated)
+            printf("VACATED\n");
+        else
+            printf("%d\n", *((int *)htbl->table[i]));
+    }
+}
+
+int main()
+{
+    OHTbl *htbl = (OHTbl *)malloc(sizeof(OHTbl));
+    ohtbl_init(htbl, O_HASHTBL_POSITIONS, hash1, hash2, match, free);
+    ohtbl_print(htbl); 
+
+    return 0;
+}
---
+++ linkedlist/ohtbl.h
@@ -0,0 +1,36 @@
+#ifndef OHTBL_H
+#define OHTBL_H
+
+#include <stdlib.h>
+
+typedef struct OHTbl_
+{
+    int positions;  // 分配的槽数目
+    void *vacated;  // 替代 NULL 表示空
+
+    int (*h1)(const void *key);
+    int (*h2)(const void *key);
+    int (*match)(const void *key1, const void *key2);
+    void (*destroy)(void *data);
+
+    int size;
+    void **table;
+} OHTbl;
+
+int ohtbl_init(
+        OHTbl *htbl,
+        int positions,
+        int (*h1)(const void *key),
+        int (*h2)(const void *key),
+        int (*match)(const void *key1, const void *key2),
+        void (*destroy)(void *data));
+
+void ohtbl_destroy(OHTbl *htbl);
+int ohtbl_insert(OHTbl *htbl, const void *data);
+int ohtbl_remove(OHTbl *htbl, void **data);
+int ohtbl_lookup(const OHTbl *htbl, void **data);
+
+#define ohtbl_size(htbl) (htbl->size)
+
+
+#endif
