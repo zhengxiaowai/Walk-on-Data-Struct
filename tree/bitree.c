3e48e48d8ad1913d3fb163481a8d6bfa885a1d70
zhengxiaowai <h1x2y3awalm@gmail.com>
2017-05-31 18:57:17 +0800

    bitree

---
+++ tree/bitree.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "bitree.h"
+
+
+void bitree_init(BiTree *tree, void (*destroy)(void *data))
+{
+
+}
+void bitree_destroy(BiTree *tree)
+{
+
+}
+int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data)
+{
+
+}
+int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data)
+{
+
+}
+void bitree_rem_left(BiTree *tree, BiTreeNode *node)
+{
+
+}
+void bitree_rem_right(BiTree *tree, BiTreeNode *node)
+{
+
+}
+void bitree_merge(BiTree *merge, BiTree *left, BiTree *right, void *data)
+{
+
+}
